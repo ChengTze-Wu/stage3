c37934b60cd065a9fda585f73815530b6082df0a
ChengTze-Wu <yaoop3050777@gmail.com>
2022-05-07 13:28:03 +0800

    add loading test

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/loaderio-7d439812d414b477181647566d228996.html", methods=["GET"])
+def loaderio():
+    return render_template('loaderio-7d439812d414b477181647566d228996.html')
+
 app.register_blueprint(api.downloader, url_prefix="/api")
 app.register_blueprint(api.uploader, url_prefix="/api")
